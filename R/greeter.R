d823fdabd9c4992ce0b736bfddeb40742ce4d0e7
Shelmith-Kariuki <kariukishelmith@gmail.com>
2020-08-14 01:45:54 +0300

    Initial commit

---
+++ R/greeter.R
@@ -0,0 +1,13 @@
+#' Title
+#'
+#' @param name A character string indicating the name of the 2nd party
+#'
+#' @return A statement that greets a person
+#' @export
+#'
+#' @examples
+#' greeter("Shel")
+greeter <- function(name){
+  paste("Hi" , name, ", I hope you are doing well.")
+}
+
